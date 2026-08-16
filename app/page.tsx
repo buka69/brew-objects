886a6f66cbadcb3f3915ef55e7f2d809d889c9ec
buka69 <ramazanov.iv@gmail.com>
2026-08-17 01:03:46 +0200

    Handle cart and wholesale links from SEO pages

--- app/page.tsx
+++ app/page.tsx
@@ -5,5 +5,6 @@ import SeoFooterLinks from '../components/SeoFooterLinks';
 import ProductModalUrlSync from '../components/ProductModalUrlSync';
 import FilterClearFix from '../components/FilterClearFix';
 import CategoryCircleNavigation from '../components/CategoryCircleNavigation';
+import HomeHashActions from '../components/HomeHashActions';
 
-export default function Page(){return <><EscapeClose/><ProductModalUrlSync/><FilterClearFix/><CategoryCircleNavigation/><Store40/><SeoHomeContent/><SeoFooterLinks/></>}
+export default function Page(){return <><EscapeClose/><ProductModalUrlSync/><FilterClearFix/><CategoryCircleNavigation/><HomeHashActions/><Store40/><SeoHomeContent/><SeoFooterLinks/></>}
