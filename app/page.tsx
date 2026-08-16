4c559ec3f62b3c28ea729435f8ace2e495afd2fb
buka69 <ramazanov.iv@gmail.com>
2026-08-17 00:48:59 +0200

    Wire Clear all filter reset fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,6 @@ import Store40 from '../components/Store40';
 import EscapeClose from '../components/EscapeClose';
 import SeoHomeContent from '../components/SeoHomeContent';
 import ProductModalUrlSync from '../components/ProductModalUrlSync';
+import FilterClearFix from '../components/FilterClearFix';
 
-export default function Page(){return <><EscapeClose/><ProductModalUrlSync/><Store40/><SeoHomeContent/></>}
+export default function Page(){return <><EscapeClose/><ProductModalUrlSync/><FilterClearFix/><Store40/><SeoHomeContent/></>}
