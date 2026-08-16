bb1d79c6d9f93971c0defec9c6213546665bd222
buka69 <ramazanov.iv@gmail.com>
2026-08-17 00:38:40 +0200

    Update storefront URL when product modal opens

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Store40 from '../components/Store40';
 import EscapeClose from '../components/EscapeClose';
 import SeoHomeContent from '../components/SeoHomeContent';
+import ProductModalUrlSync from '../components/ProductModalUrlSync';
 
-export default function Page(){return <><EscapeClose/><Store40/><SeoHomeContent/></>}
+export default function Page(){return <><EscapeClose/><ProductModalUrlSync/><Store40/><SeoHomeContent/></>}
