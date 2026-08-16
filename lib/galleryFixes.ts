0dbd5b79b01b57727397e2b7bf93f280b304dd1d
buka69 <ramazanov.iv@gmail.com>
2026-08-16 20:27:26 +0200

    Fix Cafetto gallery with four distinct product views

--- lib/galleryFixes.ts
+++ lib/galleryFixes.ts
@@ -12,6 +12,12 @@ export const galleryFixes:Record<string,ProductGallery>={
   'https://area51coffeeroasters.com/wp-content/uploads/Gear-CafelatMicrofiberCloths_4pack-P01-1080w.png',
   'https://area51coffeeroasters.com/wp-content/uploads/Gear-CafelatMicrofiberCloths_4pack-SL01-1080w.png',
   'https://area51coffeeroasters.com/wp-content/uploads/Gear-CafelatMicrofiberCloths_4pack-SL02-1080w.png'
+ ],
+ 'cafetto-gc2':[
+  'https://ineffablecoffee.com/cdn/shop/files/cafetto-gc2-450g.webp?crop=center&height=900&v=1750680228&width=900',
+  'https://lazenskakava.s24.cdn-upgates.com/_cache/b/a/bace8a235df2ac2bef92996270c5b2b0-cafetto-grinder-clean-450g-cistic-mlynku.jpg',
+  'https://lazenskakava.s24.cdn-upgates.com/_cache/d/0/d080b2f9fc53423b684c4724dd467f38-cafetto-grinder-clean-450g-cistic-mlynku.webp',
+  'https://lazenskakava.s24.cdn-upgates.com/_cache/e/e/ee05b3626f3d92251b0f9cf916211771-cafetto-grinder-clean-450g-cistic-mlynku.webp'
  ]
 };
 
